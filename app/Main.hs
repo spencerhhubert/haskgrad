28bfe36dec72827563ed77b777e25452fc7eaf50
spencerhhubert <spencerhhubert@gmail.com>
2022-06-28 20:07:11 -0400

    junk

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,20 @@
 module Main where
 
 import Haskgrad
-import Linear
+import Vector
+import Matrix
   
-vec1 = [11..15]
-vec3 = [101..105]
-
-mat4 = [vec1, vec3]
-
-mat5 = generate_matrix 100 42 6.9
-mat7 = multiply mat4 mat5
-
 x = [1..4]
-w1 = generate_matrix 2 4
-z1 = w1 `dot` x + b 
+b1 = [1..2]
+w1 = generateFloatMatrix 2 4
+z1 = w1 `dot` x + b1
 a1 = map sigmoid input_layer
 
 
 
 
 main :: IO ()
-main = print $ show hidden_layer1
+main = print $ show "ok"
 
 --let's learn a simple function
 float_vector = [0.00,0.01..10.00]
--- src/Haskgrad.hs
+++ src/Haskgrad.hs
@@ -3,3 +3,6 @@ module Haskgrad where
 sigmoid :: Float -> Float
 sigmoid x = 1 / (1 + (exp (-x)))
 
+sigmoidPrime :: Float -> Float
+sigmoidPrime x = (exp x) / (((exp x) + 1)^2)
+
